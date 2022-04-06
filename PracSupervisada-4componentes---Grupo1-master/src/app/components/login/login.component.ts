04039fcc40b90ff4886667a3ec1a1d6c7adde14e
Guídeon Caleb Morataya Hernández <88805651+gmorataya2020221@users.noreply.github.com>
2022-04-06 16:35:18 -0600

    login

--- PracSupervisada-4componentes---Grupo1-master/src/app/components/login/login.component.ts
+++ PracSupervisada-4componentes---Grupo1-master/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 
 
@@ -6,14 +8,49 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  providers: [UsuarioService]
   //encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public usuarioModel: Usuario;
+
+  constructor(private _usuarioService: UsuarioService) {
+    this.usuarioModel = new Usuario(
+    '',
+    '',
+    '',
+    '',
+    ''
+    );
+  }
 
   ngOnInit(): void {
   }
 
+  getToken(){
+    this._usuarioService.login(this.usuarioModel, "true").subscribe(
+      (response)=>{
+        console.log(response.token);
+        localStorage.setItem("token", response.token)
+      },
+      (error)=>{
+        console.log(<any>error);
 
+      }
+    )
+  }
+
+  login(){
+    this._usuarioService.login(this.usuarioModel).subscribe(
+      (response)=>{
+        console.log(response.usuario);
+        localStorage.setItem('identidad', JSON.stringify(response.usuario))
+        this.getToken();
+      },
+      (error)=>{
+        console.log(<any>error);
+      }
+    );
+  }
 }
